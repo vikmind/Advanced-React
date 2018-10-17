785e36e6a45a03c25a9e73e91fcf3376d38ef585
Viktor Marchenko <viktor.marchenko@gmail.com>
2018-10-17 10:18:08 +0300

    3

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <div>Hello!</div>
+    <Link href="/sell">Sell</Link>
+  </div>
+)
+
+export default Home;
+
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <h1>Sell!</h1>
+    <Link href="/">home</Link>
+  </div>
+)
+
+export default Sell;
