5a878889d397852607a822e1d923b2999b556df8
Viktor Marchenko <viktor.marchenko@gmail.com>
2018-10-27 15:02:28 +0300

    18

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -39,6 +39,23 @@ class CreateItem extends Component {
     const { name, type, value } = e.target;
     this.setState({ [name]: type === 'number' ? (parseFloat(value) || '') : value })
   }
+  uploadFile = async e => {
+    console.log('Uploading', e);
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'rtgli0ni');
+    const res = await fetch('https://api.cloudinary.com/v1_1/vikmind/image/upload', {
+      method: 'POST',
+      body: data,
+    });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  }
   render() {
     const LabelWithInput = ({ type, name, placeholder, required }) =>
       <label htmlFor={ name }>
@@ -78,6 +95,18 @@ class CreateItem extends Component {
           } }>
             <ErrorMessage error={ error }/>
             <fieldset disabled={ loading } aria-busy={ loading }>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Select file"
+                  required
+                  onChange={ this.uploadFile }
+                />
+                { this.state.image && <img width="200" src={ this.state.image } alt={ this.state.title } /> }
+              </label>
               { LabelWithInput({ type: 'text', name: 'title', placeholder: 'Title', required: true }) }
               { LabelWithInput({ type: 'number', name: 'price', placeholder: 'Price', required: true }) }
               { LabelWithInput({ type: 'textarea', name: 'description', placeholder: 'Description', required: true }) }
